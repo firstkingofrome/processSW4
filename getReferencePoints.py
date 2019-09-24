cfc1f4271c227efa7f11e8bf7d947092387d7000
Eric Eckert <eeckert@seismo4.lbl.gov>
2019-09-24 09:30:56 -0700

    modified the getReferencePoints code after a consulation with Suiwen that identified a coordinate mismatch

--- getReferencePoints.py
+++ getReferencePoints.py
@@ -17,43 +17,42 @@ sw4_j_start = 18000
 sw4_j_end   = 18200
 sw4_k_start = 0
 sw4_k_end   = 200
-sw4_grid_spacing=5
+
 ESSI_SURFACE = 200
 
-
-
-
-
-sw4essiout_filename =  "Location5.cycle=00000.essi"
-pointsRequested = "outputLocations.csv"
+sw4essiout_filename =  "testContainer.essi"
+pointsRequested = "comparisionLocation.csv"
 pointsOutput = "outputPoints"
 #load the csv file
 points = pandas.read_csv(pointsRequested)
-
-
 #open the hdf5 container for this run and get each out put velocity point
+
 sw4essiout = h5py.File(sw4essiout_filename, 'r')
-spacing = sw4essiout['ESSI xyz grid spacing'][:][0]
+spacing = int(sw4essiout['ESSI xyz grid spacing'][:][0])
+#get constant values
+sw4Xstart,sw4Ystart,sw4Zstart = int(points["sw4Xstart"][0]),int(points["sw4Ystart"][0]),int(points["sw4Zstart"][0])
+openSeesxStart,openSeesyStart,openSeeszStart = int(points["openSeesxStart"][0]),int(points["openSeesyStart"][0]),int(points["openSeeszstart"][0])
 for index, row in points.iterrows():
     print(row)
     #translate into xyz coordinates
     ### THE ESSI CORDINATES ARE XYZ WHILE SW4 IS YXZ
-    i,j,k = row["x (m)"],row["y (m)"],row["z (m)"]
+    OSx,OSy,OSz = row["x"],row["y"],row["z"]
     print("preparing refrence point " + row["point"])
-    print("saving x y and z velocities for  exssi point " + "%i,%i,%i" % (i,j,k))
-    j,i,k = int((i)/spacing), int((j)/spacing),int((ESSI_SURFACE-k)/spacing)
-    print("This corresponds to sw4 point %i,%i,%i"  %(i,j,k))
+    print("saving x y and z velocities for  exssi point " + "%i,%i,%i" % (OSx,OSy,OSz))
+    """
+    THE LINE BELOW THIS IS WHERE I CONVERT FROM ESSI --> SW4
+    """
+    sw4x,sw4y,sw4z = (sw4Xstart+(OSy-openSeesyStart))/spacing,(sw4Ystart+(OSx-openSeesxStart))/spacing,(sw4Zstart+(openSeeszStart-OSz))/spacing
+    print("This corresponds to sw4 index %i,%i,%i"  %(sw4x,sw4y,sw4z))
     print('\n')
-    
     #save xyz as csv files for that point
     #save ESSI x y and z!
-    
     fname = "_vel_sw4_"+row["point"]+".csv"
-    x = sw4essiout['vel_1 ijk layout'][:,i,j,k]
+    x = sw4essiout['vel_1 ijk layout'][:,sw4x,sw4y,sw4z]
     np.savetxt(fname='x'+fname,X=x)
     #save y
-    y = sw4essiout['vel_0 ijk layout'][:,i,j,k]
+    y = sw4essiout['vel_0 ijk layout'][:,sw4x,sw4y,sw4z]
     np.savetxt(fname='y'+fname,X=y)
     #save z
-    z = -1.0*sw4essiout['vel_2 ijk layout'][:,i,j,k]
+    z = -1.0*sw4essiout['vel_2 ijk layout'][:,sw4x,sw4y,sw4z]
     np.savetxt(fname='z'+fname,X=z)
